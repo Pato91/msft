bc150121d5cff235a710c5ca45e26a78c647d1e0
jodom <jodomouma@gmail.com>
2017-12-02 09:33:13 +0300

    rotate matrix inplace

--- leet/rotatematrix.py
+++ leet/rotatematrix.py
@@ -24,6 +24,24 @@ def cRotate(M):
 		MP.append(R)
 	return MP
 
+def iRotate(matrix):
+	""" rotate a matrix M 90 degrees clockwise, inplace """
+	n = len(matrix) # row count of the n*n matrix
+
+	for layer in range(n//2): # iterate per layer, inner most layer is n//2
+
+		first, last = layer, n-layer-1 # first and last positions in a layer
+
+		for i in range(first, last):
+			top = matrix[layer][i] # save the top position
+			matrix[layer][i] = matrix[-i-1][layer]
+			matrix[-i-1][layer] = matrix[-i-1][-layer-1]
+			matrix[-i-1][-layer-1] = matrix[i][-layer-1]
+			matrix[i][-layer-1] = top
+
+	return matrix
+
+
 class TestRotate(unittest.TestCase):
 	data = (
 		[
@@ -48,10 +66,16 @@ class TestRotate(unittest.TestCase):
 		expected = self.data[1]
 		self.assertEqual(rotated, expected)
 
+	def test_rotate_clockwise_inplace(self):
+		rotated = iRotate(self.data[0])
+		expected = self.data[1]
+		self.assertEqual(rotated, expected)
+
 	def test_rotate_anticlockwise(self):
 		rotated = aRotate(self.data[0])
 		expected = self.data[2]
 		self.assertEqual(rotated, expected)
+
 
 if __name__ == '__main__':
 	unittest.main()
